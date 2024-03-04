838e9dafa0993b9e667c7f19de4d0f27b307cba8
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-04 17:23:38 +0100

    add cart icon function to the navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,161 @@
 
-"use client"
+// "use client"
+
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import React, { useState } from 'react';
+
+// const Navbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleNavbar = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const closeNavbar = () => {
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <nav className=" shadow-lg py-4 md:px-20 fixed top-0 w-full z-50 backdrop-filter backdrop-blur-lg">
+//       <div className="container mx-auto px-4 flex justify-between items-center">
+//         <Link href="/" className="flex items-center">
+//           <Image src={require("../assets/logo.png")} alt='' width={220} height={60} />
+//         </Link>
+//         <div className="hidden md:flex items-center space-x-4">
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Home</a>
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Products</a>
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">About</a>
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Contact</a>
+//         </div>
+//         {/* <div className="md:flex items-center space-x-4">
+//           <button className="bg-[#1941e1] hover:bg-[#19d7b4] text-white py-2 px-4 rounded-md">Sign In</button>
+//           <button className="bg-[#19d7b4] hover:bg-[#1941e1] text-white py-2 px-4 rounded-md">Sign Up</button>
+//         </div> */}
+//         <div className="md:hidden">
+//           <button
+//             onClick={toggleNavbar}
+//             className="text-[#1941e1] focus:outline-none focus:text-gray-300"
+//           >
+//             <svg
+//               className="h-6 w-6"
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               stroke="currentColor"
+//             >
+//               {isOpen ? (
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth={2}
+//                   d="M6 18L18 6M6 6l12 12"
+//                 />
+//               ) : (
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth={2}
+//                   d="M4 6h16M4 12h16m-7 6h7"
+//                 />
+//               )}
+//             </svg>
+//           </button>
+//         </div>
+//       </div>
+//       {isOpen && (
+//         <div className="absolute  top-16 left-0 w-full  md:hidden flex justify-end">
+//           <div className="px-2 py-4 space-y-2 shadow backdrop-filter backdrop-blur-lg bg-slate-100 w-3/6 text-center rounded-t-lg">
+//             <a href="#" className="block hover:text-[#19d7b4]" onClick={closeNavbar}>Home</a>
+//             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>Products</a>
+//             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>About</a>
+//             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>Contact</a>
+//           </div>
+//         </div>
+//       )}
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import React, { useState } from 'react';
+// import { FaShoppingCart } from 'react-icons/fa';
+// import { HiMenuAlt3 } from "react-icons/hi";
+// import { IoClose } from "react-icons/io5";
+// const Navbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [cartItems, setCartItems] = useState([]);
+
+//   const toggleNavbar = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const closeNavbar = () => {
+//     setIsOpen(false);
+//   };
+
+//   return (
+//     <nav className="shadow-lg py-4 md:px-20 fixed top-0 w-full z-50 backdrop-filter backdrop-blur-lg">
+//       <div className="container mx-auto px-4 flex justify-between items-center">
+//         <Link href="/" className="flex items-center">
+//           <Image src={require("../assets/logo.png")} alt='' width={220} height={60} />
+//         </Link>
+//         <div className="hidden md:flex items-center space-x-4">
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Home</a>
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Products</a>
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">About</a>
+//           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Contact</a>
+//         </div>
+//         <div className="md:hidden">
+//           <button
+//             onClick={toggleNavbar}
+//             className="text-[#1941e1] focus:outline-none focus:text-gray-300"
+//           >
+
+//             {isOpen ? (
+//               <IoClose size={40} />
+//             ) : (
+
+//               <HiMenuAlt3 size={40} />
+//             )}
+
+//           </button>
+//         </div>
+//         <div className="">
+//         <sub>  <FaShoppingCart size={24} className="text-[#1941e1] cursor-pointer" /></sub>
+//           <sup className="text-[#1941e1] bg-white font-bold p-2 rounded-full">0</sup>
+//         </div>
+//       </div>
+//       {isOpen && (
+//         <div className="absolute  top-16 left-0 w-full  md:hidden flex justify-end">
+//           <div className="px-2 py-4 space-y-2 shadow backdrop-filter backdrop-blur-lg bg-slate-100 w-3/6 text-center rounded-t-lg">
+//             <a href="#" className="block hover:text-[#19d7b4]" onClick={closeNavbar}>Home</a>
+//             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>Products</a>
+//             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>About</a>
+//             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>Contact</a>
+//           </div>
+//         </div>
+//       )}
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
 
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
@@ -17,7 +166,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className=" shadow-lg py-4 md:px-20 fixed top-0 w-full z-50 backdrop-filter backdrop-blur-lg">
+    <nav className="shadow-lg py-4 md:px-20 fixed top-0 w-full z-50 backdrop-filter backdrop-blur-lg">
       <div className="container mx-auto px-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
           <Image src={require("../assets/logo.png")} alt='' width={220} height={60} />
@@ -28,43 +177,26 @@ const Navbar = () => {
           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">About</a>
           <a href="#" className="text-[#1941e1] hover:text-[#19d7b4]">Contact</a>
         </div>
-        {/* <div className="md:flex items-center space-x-4">
-          <button className="bg-[#1941e1] hover:bg-[#19d7b4] text-white py-2 px-4 rounded-md">Sign In</button>
-          <button className="bg-[#19d7b4] hover:bg-[#1941e1] text-white py-2 px-4 rounded-md">Sign Up</button>
-        </div> */}
         <div className="md:hidden">
           <button
             onClick={toggleNavbar}
             className="text-[#1941e1] focus:outline-none focus:text-gray-300"
           >
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {isOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              )}
-            </svg>
+            {isOpen ? (
+              <IoClose size={40} />
+            ) : (
+              <HiMenuAlt3 size={40} />
+            )}
           </button>
+        </div>
+        <div className="relative">
+          <sup className="text-[#1941e1] bg-white font-bold p-2 rounded-full absolute -top-2 -right-2">{cartItems.length}</sup>
+          <FaShoppingCart size={24} className="text-[#1941e1] cursor-pointer" />
         </div>
       </div>
       {isOpen && (
         <div className="absolute  top-16 left-0 w-full  md:hidden flex justify-end">
-          <div className="px-2 py-4 space-y-2 shadow backdrop-filter backdrop-blur-lg bg-slate-100 w-3/6 text-center rounded-t-lg">
+          <div className="px-2 py-4 space-y-10 shadow backdrop-filter backdrop-blur-lg bg-slate-100 w-4/6 text-center rounded-t-lg ">
             <a href="#" className="block hover:text-[#19d7b4]" onClick={closeNavbar}>Home</a>
             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>Products</a>
             <a href="#" className="block  hover:text-[#19d7b4]" onClick={closeNavbar}>About</a>
--- src/app/components/ProductSection.tsx
+++ src/app/components/ProductSection.tsx
@@ -59,8 +59,7 @@ const ProductSection = () => {
                         <option value="">All Categories</option>
                         {/* Add category options here */}
                     </select>
-                    <input type="range" min="0" max="100" className="mr-4" />
-                    <span className="text-gray-700">Price Range</span>
+
                 </div>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
