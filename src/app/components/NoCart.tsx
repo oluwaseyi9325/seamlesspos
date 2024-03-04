9bd786c1d54d74a6cef743b7f028b1019757ad60
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-04 16:49:50 +0100

    create not-found page

--- src/app/components/NoCart.tsx
+++ src/app/components/NoCart.tsx
@@ -1,20 +1,23 @@
-import Image from 'next/image'
-import React from 'react'
+import React from 'react';
 import { BsCartXFill } from "react-icons/bs";
+
 function NoCart() {
     return (
-        <div className=''>
-            <div className='flex justify-center  my-10 '>
-                <section className='bg-white shadow p-10 text-center'>
+        <div className='container mx-auto px-3 md:px-[100px]'>
+            <div className='flex justify-center my-10'>
+                <section className='bg-white shadow-lg rounded-lg p-10 text-center'>
                     <div className='flex justify-center'>
                         <BsCartXFill size={80} color='red' />
                     </div>
                     <h3 className='font-bold my-5 text-2xl'>Your cart is empty.</h3>
-                    <div className='text-slate-800'>You have not added any item to your cart.</div>
+                    <p className='text-gray-600 mb-5'>Looks like you haven't added any items to your cart yet.</p>
+                    <button className='bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 focus:outline-none'>
+                        Explore Products
+                    </button>
                 </section>
             </div>
         </div>
-    )
+    );
 }
 
-export default NoCart
+export default NoCart;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+function NotFoundPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <div className='bg-white text-center p-10'>
+                <Image src={require("./assets/notFound.gif")} alt="404 Not Found" width={400} height={300} />
+
+                <h2 className="text-3xl font-bold text-gray-800 mt-8 mb-4">Oops! Page Not Found</h2>
+                <p className="text-lg text-gray-600 mb-8">The page you are looking for might have been removed,<br /> had its name changed, or is temporarily unavailable.</p>
+
+                <Link href="/" className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 focus:outline-none">
+                    Go Back to Home
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default NotFoundPage;
