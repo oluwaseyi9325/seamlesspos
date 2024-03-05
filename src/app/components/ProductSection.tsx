50a33ce1afea32c3a9861ccfab56c1337f3302aa
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-05 18:54:48 +0100

    logic code for search produts

--- src/app/components/ProductSection.tsx
+++ src/app/components/ProductSection.tsx
@@ -7,7 +7,9 @@ const ProductSection = ({showStatus}:any) => {
     const [products, setProducts] = useState<ProductInterface[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+    const [searchQuery, setSearchQuery] = useState<string>('');
     const [showNumber, setShowNumber] = useState<number | 'all'>(showStatus); 
+    const [filteredProducts,setFilter]=useState<any>()
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -15,6 +17,7 @@ const ProductSection = ({showStatus}:any) => {
                 const data = await response.json();
                 setProducts(data);
                 setLoading(false);
+                setFilter(showNumber === 'all' ? products : products.slice(0, showNumber)); 
             } catch (error) {
                 setError('Error fetching data');
                 setLoading(false);
@@ -23,7 +26,17 @@ const ProductSection = ({showStatus}:any) => {
 
         fetchData();
     }, [products]);
-    const filteredProducts = showNumber === 'all' ? products : products.slice(0, showNumber); 
+   
+
+    const searchBtn=()=>{
+     setProducts(products.filter((product) =>
+        product.name.toLowerCase().includes(searchQuery.toLowerCase())
+    ));
+    console.log(filteredProducts)
+   
+    }
+
+   
     if (loading) {
         return <div className='flex justify-center'>Loading...</div>;
     }
@@ -36,6 +49,17 @@ const ProductSection = ({showStatus}:any) => {
         <section className="container mx-auto py-12 px-10 md:px-20">
             <div className="flex justify-between items-center mb-6">
                 <h2 className="text-2xl font-bold text-gray-800">Products</h2>
+                <div>
+                    <span className='bg-white py-4 px-3 rounded-lg'>
+                    <input
+                        placeholder='Search'
+                        className='rounded focus:none'
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)} 
+                    />
+                    <button onClick={searchBtn} className='bg-gray-800 p-2 text-white rounded-lg'>Seacrh</button>
+                    </span>
+                </div>
                 <div>
                     <select className="px-4 py-2 mr-4 bg-gray-200 text-gray-800 rounded">
                         <option value="">All Categories</option>
@@ -56,3 +80,7 @@ const ProductSection = ({showStatus}:any) => {
 };
 
 export default ProductSection;
+function fetchData() {
+    throw new Error('Function not implemented.');
+}
+
