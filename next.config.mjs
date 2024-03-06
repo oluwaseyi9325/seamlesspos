473c9bea342fb1eff5829803b79f9749309fb24a
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-06 13:50:20 +0100

    add mock api image for the project in the data.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['via.placeholder.com','images.unsplash.com'],
+        domains: ['via.placeholder.com','images.unsplash.com',"plus.unsplash.com"],
     },
 };
 
